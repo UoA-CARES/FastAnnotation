e9539e9edcaaf4e086ec6c1cc79bd72875511602
Arran Davis <arran94@gmail.com>
2020-01-30 17:13:46 +1300

    Add Project POST method to server

--- database/database.py
+++ database/database.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector.errors import InterfaceError
 
 
 class Database:
@@ -9,37 +10,77 @@ class Database:
             host=self.config.DATABASE_HOST,
             user=self.config.DATABASE_USER,
             passwd=self.config.DATABASE_PASSWORD,
-            database=self.config.DATABASE_NAME
         )
         self.db_cursor = self.db.cursor()
         self._generate_table_types()
+        self.db_cursor.close()
+        self.db.close()
 
-    def query(self, query_string):
-        self.db_cursor.execute(query_string)
-        return self.db_cursor.fetchall()
+        self.db = mysql.connector.connect(
+            host=self.config.DATABASE_HOST,
+            user=self.config.DATABASE_USER,
+            passwd=self.config.DATABASE_PASSWORD,
+            database=self.config.DATABASE_NAME
+        )
+        self.db.autocommit = True
+        self.db_cursor = self.db.cursor()
 
-    def form_results(self, table_name, results):
+    def get_last_row_id(self):
+        return self.db_cursor.lastrowid
+
+    def query(self, query_string, params=None):
+        self.db_cursor.execute(query_string, params)
+        try:
+            return self.db_cursor.fetchall()
+        except InterfaceError:
+            return []
+
+    def rows_to_json(self, table_name, rows):
         if table_name not in self.table_types:
             return None
 
+        if not isinstance(rows, list):
+            rows = [rows]
+
         column_names = self.table_types[table_name]
-        output = {"results": []}
-        for result in results:
-            row = {}
+        output = []
+        for row in rows:
+            json = {}
+            assert len(row) is len(column_names), "Incoming row has %d fields when it is expected to have %d." % (
+                len(row), len(column_names))
             for i in range(len(column_names)):
-                row[column_names[i]] = result[i]
-            output["results"].append(row)
+                json[column_names[i]] = row[i]
+            output.append(json)
+        return output
+
+    def json_to_rows(self, table_name, json):
+        if table_name not in self.table_types:
+            return None
+
+        if not isinstance(json, list):
+            json = [json]
+
+        column_names = self.table_types[table_name]
+        output = []
+        for item in json:
+            row = []
+            for i in range(len(column_names)):
+                assert column_names[i] in item, "Incoming JSON Item is missing key '%s'." % (
+                    column_names[i])
+                row[i] = item[column_names[i]]
+            output.append(tuple(row))
         return output
 
     def _generate_table_types(self):
         self.table_types = {}
         query = "SELECT DISTINCT TABLE_NAME FROM INFORMATION_SCHEMA.COLUMNS "
-        query += "WHERE TABLE_SCHEMA = \"" + self.config.DATABASE_NAME + "\""
-        table_names = [x[0] for x in self.query(query)]
+        query += "WHERE TABLE_SCHEMA = %s"
+        table_names = [x[0]
+                       for x in self.query(query, (self.config.DATABASE_NAME,))]
         for name in table_names:
             query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS "
-            query += "WHERE TABLE_NAME = \"" + name + "\" "
+            query += "WHERE TABLE_NAME = \"%s\" "
             query += "ORDER BY ORDINAL_POSITION"
-            column_names = [x[0] for x in self.query(query)]
+            column_names = [x[0] for x in self.query(query, (name,))]
             self.table_types[name] = column_names
         return
--- server/main.py
+++ server/main.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
+from flask import abort
+from flask_negotiate import produces, consumes
 from datetime import datetime
 from server.server_config import ServerConfig
 from database.database import Database
-
+from mysql.connector.errors import IntegrityError
 
 app = Flask(__name__)
 
@@ -17,6 +20,7 @@ def heartbeat():
 
 
 @app.route("/project", methods=['GET'])
+@produces('application/json')
 def get_projects():
     results = fadb.query("SELECT project_id from project")
     results = [x[0] for x in results]
@@ -26,11 +30,53 @@ def get_projects():
 
 
 @app.route("/project/<int:id>", methods=['GET'])
+@produces('application/json')
 def get_project(id):
     query = "SELECT * from project "
-    query += "WHERE project_id = " + str(id)
-    results = fadb.query(query)
-    return jsonify(fadb.form_results('project', results))
+    query += "WHERE project_id = %s"
+    results = fadb.query(query, (id,))
+    return jsonify(fadb.rows_to_json('project', results))
+
+
+@app.route("/project", methods=['POST'])
+@consumes('application/json')
+def add_project():
+    content = request.get_json()
+    if not isinstance(content, list):
+        content = [content]
+
+    success_ids = []
+    error_msgs = []
+    for item in content:
+        query = "INSERT INTO project ("
+        query += ",".join(item.keys())
+        query += ") "
+        query += "VALUES ("
+        query += ",".join(["%s"] * len(item))
+        query += ");"
+        try:
+            fadb.query(query, tuple(item.values()))
+            id = fadb.get_last_row_id()
+            success_ids.append(id)
+        except IntegrityError as e:
+            error_msgs.append(
+                {"request": item, "err_code": e.errno, "err_msg": e.msg})
+        except BaseException:
+            error_msgs.append(
+                {"request": item, "err_code": 500, "err_msg": "Unknown Server Fault."})
+
+    if not error_msgs:
+        response = jsonify({"Created Ids": success_ids})
+        response.status_code = 201
+    elif not success_ids:
+        response = jsonify({"Bad Requests": error_msgs})
+        response.status_code = 400
+    else:
+        response = jsonify({"Created Ids": success_ids,
+                            "Bad Requests": error_msgs})
+        response.status_code = 201
+
+    return response
 
 
 if __name__ == "__main__":
