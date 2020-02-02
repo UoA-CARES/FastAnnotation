c783baac9bb313dd15d22e889340cc9e479bbc11
PastaTime <arran94@gmail.com>
2020-02-03 10:49:40 +1300

    Add Schema creation to database creation script

--- database/create_database.sql
+++ database/create_database.sql
@@ -1,4 +1,6 @@
-CREATE TABLE `project` (
+CREATE SCHEMA `fadb` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `fadb`.`project` (
   `project_id` int NOT NULL AUTO_INCREMENT,
   `project_name` varchar(80) NOT NULL,
   `last_uploaded` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -8,5 +10,5 @@ CREATE TABLE `project` (
   UNIQUE KEY `project_id_UNIQUE` (`project_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `` (`project_id`,`project_name`,`last_uploaded`,`unlabeled_count`,`labeled_count`) VALUES (1,'Test','2020-01-23 23:29:38',0,0);
-INSERT INTO `` (`project_id`,`project_name`,`last_uploaded`,`unlabeled_count`,`labeled_count`) VALUES (2,'Test2','2020-01-23 23:31:12',0,0);
+INSERT INTO `fadb`.`project` (`project_id`,`project_name`,`last_uploaded`,`unlabeled_count`,`labeled_count`) VALUES (1,'Test','2020-01-23 23:29:38',0,0);
+INSERT INTO `fadb`.`project` (`project_id`,`project_name`,`last_uploaded`,`unlabeled_count`,`labeled_count`) VALUES (2,'Test2','2020-01-23 23:31:12',0,0);
