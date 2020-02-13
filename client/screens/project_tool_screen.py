ff77e37b2ef3ed8b9a038c6eb0f5b96c0e3a7e8e
Arran Davis <arran94@gmail.com>
2020-02-14 12:27:09 +1300

    Add file dialog

--- client/screens/project_tool_screen.py
+++ client/screens/project_tool_screen.py
@@ -1,9 +1,22 @@
 from client.screens.common import *
 from kivy.uix.screenmanager import Screen
 
+from client.definitions import ROOT_DIR
+import tkinter as tk
+from tkinter import filedialog
+
+
 # Load corresponding kivy file
 Builder.load_file(os.path.join(SCREEN_DIR, 'project_tool_screen.kv'))
 
 
 class ProjectToolScreen(Screen):
-    pass
+    def open_file_chooser(self, *args):
+        root = tk.Tk()
+        root.withdraw()
+        file_path = filedialog.askdirectory(initialdir=ROOT_DIR)
+        print(file_path)
+
+
+class FileChooserPopup(Popup):
+    default_path = StringProperty("")
