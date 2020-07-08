Fix deadzones when scaling images

client/paintapp.py
@@ -8,7 +8,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.relativelayout import RelativeLayout
 
 from client.screens.common import MouseDrawnTool
-from client.screens.paint_window import PaintWindow
+from client.screens.paint_window import PaintWindow2
 
 
 class PaintApp(App):
@@ -20,12 +20,12 @@ class Test(RelativeLayout):
     def __init__(self, **kw):
         super().__init__(**kw)
         image = np.zeros(shape=(300, 200, 3), dtype=np.uint8)
-        image[:] = (255, 255, 0)
+        image[:] = (255, 0, 0)
         image[0:10, 0:10, :] = 0
         image[290:300, 0:10, :] = 80
         image[0:10, 190:200, :] = 160
         image[290:300, 190:200, :] = 240
-        pw = PaintWindow(image)
+        pw = PaintWindow2(image)
         self.add_widget(pw)
         pw.add_layer('test', [255, 255, 255])
         drawtool = DrawTool(pw)